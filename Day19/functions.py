b1d9374d31561b1195e1a24e686d5ccffc291fd7
rohit <rohit26av@gmail.com>
2024-08-27 21:57:30 +0530

    Initial Commit

---
+++ Day19/functions.py
@@ -0,0 +1,12 @@
+FILEPATH = 'todos.txt'
+
+
+def web_get_todos(filepath=FILEPATH):
+    with open("todos.txt", 'r') as file:
+        local_todos = file.readlines()
+    return local_todos
+
+
+def web_write_todos(todos_arg, filepath=FILEPATH):
+    with open("todos.txt", 'w') as file:
+        write_todos = file.writelines(todos_arg)
